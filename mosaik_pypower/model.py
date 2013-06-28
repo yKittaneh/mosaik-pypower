86a2b94b26ed580953cd4144a1ca7d996f60e368
Stefan Scherfke <stefan.scherfke@offis.de>
2013-06-28 12:50:21 +0200

    Loading input files and creating pp cases should work now. more or lessat least ...

--- mosaik_pypower/model.py
+++ mosaik_pypower/model.py
@@ -11,10 +11,31 @@ from pypower import idx_bus
 import numpy
 
 
-Bus = collections.namedtuple('Bus', 'idx')
-Generator = collections.namedtuple('Generator', 'idx, bus_id ')
-Transformer = collections.namedtuple('Transformer', 'idx, from_bus, to_bus')
-Branch = collections.namedtuple('Branch', 'idx, from_bus, to_bus')
+(
+    BUS_NAME,
+    BUS_TYPE,
+    BUS_BASE_KV,
+) = range(3)
+
+(
+    TRF_NAME,
+    TRF_FROM_BUS,
+    TRF_TO_BUS,
+    TRF_SR,
+    TRF_V1,
+    TRF_P1,
+) = range(6)
+
+(
+    BRANCH_NAME,
+    BRANCH_FROM_BUS,
+    BRANCH_TO_BUS,
+    BRANCH_L,
+    BRANCH_R,
+    BRANCH_X,
+    BRANCH_C,
+    BRACHN_IMAX,
+) = range(8)
 
 
 class UniqueKeyDict(dict):
@@ -26,6 +47,9 @@ class UniqueKeyDict(dict):
 
 
 def load_case(path):
+    """Load the case from *path* and create a PYPOWER case and an entiy map.
+
+    """
     raw_case = json.load(open(path))
     entity_map = UniqueKeyDict()
     buses, gens = _get_buses(raw_case, entity_map)
@@ -33,24 +57,30 @@ def load_case(path):
     ppc = {
         'baseMVA': raw_case['base_mva'],
         'bus': buses,
-        'gen': generators,
+        'gen': gens,
         'branch': branches,
     }
 
-    return ppc, eid_map
+    return ppc, entity_map
 
 
 def _get_buses(raw_case, entity_map):
+    """Create the PP bus and generator lists and update the *entity_map* with
+    all buses.
+
+    """
     buses = []
     gens = []
     for i, (bus_id, bus_type, base_kv) in enumerate(raw_case['bus']):
-        entity_map[bus_id] = Bus(i)
+        entity_map[bus_id] = {'etype': 'PQBus', 'idx': i, 'static': {
+            'vl': base_kv,
+        }}
 
         # Create PYPOWER bus:
-        #             id bus_type
-        #             Pd Qd Gs Bs area Vm Va baseKV   zone Vmax Vmin
-        buses.append((i, getattr(pypower.idx_bus, bus_type),
-                      0, 0, 0, 0, 1,   1, 0, base_kv, 1,   1.1, 0.9))
+        #             id bus_type                    Pd Qd Gs Bs area Vm Va
+        #             baseKV   zone Vmax Vmin
+        buses.append((i, getattr(idx_bus, bus_type), 0, 0, 0, 0, 1,   1, 0,
+                      base_kv, 1,   1.1, 0.9))
 
         if bus_type == 'REF':
             # Create generator for reference buses:
@@ -63,60 +93,82 @@ def _get_buses(raw_case, entity_map):
     return numpy.array(buses), numpy.array(gens)
 
 
-def _get_branches(raw_case, entity_map, buses):
+def _get_branches(raw_case, entity_map):
+    """Parse the transformers and branches, return the list of branches for
+    PP and update the entity_map.
+
+    """
     branches = []
-    trafo_map = {}
     base_mva = raw_case['base_mva']
+    buses = raw_case['bus']
+
+    # Load transformers
     for i, (tid, from_bus, to_bus, Sr, v1, P1) in enumerate(raw_case['trafo']):
-        if not from_bus in entiy_map or to_bus in to_bus:
+        if not from_bus in entity_map or not to_bus in entity_map:
             raise ValueError('Bus "%s" or "%s" not found.' %
                              (from_bus, to_bus))
-        entity_map[tid] = Transformer(len(branches), from_bus, to_bus)
-        from_bus = buses[entity_map[from_bus].idx]
-        to_bus = buses[entity_map[to_bus].idx]
-        branches.append(_make_transformer(from_bus, to_bus, Sr, v1, P1,
-                                          base_mva))
+
+        idx = len(branches)
+        from_bus_idx = entity_map[from_bus]['idx']
+        to_bus_idx = entity_map[to_bus]['idx']
+        from_bus = buses[from_bus_idx]  # Get bus from JSON
+        to_bus = buses[to_bus_idx]  # Get bus from JSON
+
+        # Update entity map with etype and static data
+        entity_map[tid] = {'etype': 'Transformer', 'idx': idx, 'static': {
+            's_max': Sr, 'prim_bus': from_bus[0], 'sec_bus': to_bus[0],
+        }}
+
+        branches.append(_make_transformer(from_bus_idx, to_bus_idx, Sr,
+                                          from_bus[BUS_BASE_KV],
+                                          to_bus[BUS_BASE_KV],
+                                          v1, P1, base_mva))
 
     omega = 2 * math.pi * 50  # s^-1
     for i, (bid, from_bus, to_bus, l, r, x, c, Imax) in enumerate(
-            raw_case('branch')):
-        if not from_bus in entiy_map or to_bus in to_bus:
+            raw_case['branch']):
+        if not from_bus in entity_map or not to_bus in entity_map:
             raise ValueError('Bus "%s" or "%s" not found.' %
                              (from_bus, to_bus))
-        entity_map[tid] = Branch(len(branches), from_bus, to_bus)
-        from_bus = buses[entity_map[from_bus].idx]
-        to_bus = buses[entity_map[to_bus].idx]
 
-        # Calculate Smax and b
-        base_kv = [pypower.idx_bus.BASE_KV] # kV
-        base_z = base_kv ** 2 / base_mva  # Ohm
+        idx = len(branches)
+        from_bus_idx = entity_map[from_bus]['idx']
+        to_bus_idx = entity_map[to_bus]['idx']
+        from_bus = buses[from_bus_idx]  # Get bus from JSON
+        to_bus = buses[to_bus_idx]  # Get bus from JSON
+
+        # Calculate some branch parameters
+        base_kv = from_bus[BUS_BASE_KV] # kV
         Smax = base_kv * Imax / 1000  # MVA
+        base_z = base_kv ** 2 / base_mva  # Ohm
         b = (omega * c / (10 ** 6))  # b in Ohm^-1, c is in muF
 
+        # Update entiy map
+        entity_map[bid] = {'etype': 'Branch', 'idx': idx, 'static': {
+            's_max': Smax, 'length': l,
+        }}
+
         # Create branch
-        # fbus, tbus, r, x, b, rateA, rateB, rateC,ratio, angle, status,
-        # angmin, angmax
-        branch = (from_bus[idx_bus.BUS_I], to_bus[idx_bus.BUS_I], r / base_z,
-                r / base_z, b * base_z, Sr, Sr, Sr, 0, 0, 1, -360, 360)
+        # fbus, tbus, r, x,
+        # b, rateA, rateB, rateC,ratio, angle, status, angmin, angmax
+        branch = (from_bus_idx, to_bus_idx, r * l / base_z, x * l / base_z,
+                  b * l * base_z, Smax, Smax, Smax, 0, 0, 1, -360, 360)
         branches.append(branch)
 
     return numpy.array(branches)
 
 
-def _make_transformer(from_bus, to_bus, Sr, v1, P1, base_MVA):
-    # Get primary and secondary voltage from connected buses
-    Vp = from_bus[idx_bus.BASE_KV]
-    Vs = to_bus[idx_bus.BASE_KV]
-
+def _make_transformer(from_bus, to_bus, Sr, Vp, Vs, v1, P1, base_mva):
+    """Helper to create a transformer-branch for PP."""
     # Calculate resistances
     # See: Adolf J. Schwab: Elektroenergiesysteme, pp. 385, 3rd edition, 2012
-    X01 = (u1 * (Vp ** 2)) / (100 * Sr)  # Ohm
+    X01 = (v1 * (Vp ** 2)) / (100 * Sr)  # Ohm
     R01 = (P1 * (X01 ** 2)) / ((v1 * Vp / 100) ** 2)  # Ohm
     base_z = Vp ** 2 / base_mva
 
     # Create branch
-    # fbus, tbus, r, x, b, rateA, rateB, rateC,ratio, angle, status, angmin,
-    # angmax
-    branch = (from_bus[idx_bus.BUS_I], to_bus[idx_bus.BUS_I], R01 / base_z,
-              X01 / base_z, 0, Sr, Sr, Sr, 1, 0, 1, -360, 360)
+    # fbus, tbus, r, x, b, rateA, rateB, rateC,
+    # ratio, angle, status, angmin, angmax
+    branch = (from_bus, to_bus, R01 / base_z, X01 / base_z, 0, Sr, Sr, Sr,
+              1, 0, 1, -360, 360)
     return branch
--- mosaik_pypower/test/test_model.py
+++ mosaik_pypower/test/test_model.py
@@ -1,3 +1,6 @@
+import os.path
+
+import numpy as np
 import pytest
 
 from mosaik_pypower import model
@@ -12,3 +15,48 @@ def test_uniqe_key_dict():
         pytest.fail('Expected a ValueError.')
     except KeyError:
         pass
+
+
+def test_load_case():
+    filename = os.path.join(os.path.dirname(__file__), 'data',
+                            'test_case_b.json')
+    ppc, emap = model.load_case(filename)
+
+    assert len(ppc) == 4
+    assert ppc['baseMVA'] == 1
+    assert np.all(ppc['bus'] == np.array([
+        [0., 3., 0., 0., 0., 0., 1., 1., 0., 110., 1., 1.1, 0.9],
+        [1., 1., 0., 0., 0., 0., 1., 1., 0.,  20., 1., 1.1, 0.9],
+        [2., 1., 0., 0., 0., 0., 1., 1., 0.,  20., 1., 1.1, 0.9],
+        [3., 1., 0., 0., 0., 0., 1., 1., 0.,  20., 1., 1.1, 0.9],
+        [4., 1., 0., 0., 0., 0., 1., 1., 0.,  20., 1., 1.1, 0.9],
+    ]))
+    assert np.all(ppc['gen'] == np.array([
+        [0., 0., 0., 999., -999., 1., 1., 1.,999., 0., 0., 0., 0., 0., 0., 0.,
+         0., 0., 0., 0., 0.],
+    ]))
+    assert np.all(ppc['branch'] == np.array([
+        [0., 1., 8.64375e-05, 3.2e-03, 0.00000000e+00, 40.  , 40.  , 40.,   1., 0., 1., -360, 360],
+        [1., 2., 1.56250e-03, 1.4e-03, 1.88495559e+02, 19.98, 19.98, 19.98, 0., 0., 1., -360, 360],
+        [1., 3., 9.37500e-04, 8.4e-04, 1.13097336e+02, 19.98, 19.98, 19.98, 0., 0., 1., -360, 360],
+        [2., 4., 6.25000e-04, 5.6e-04, 7.53982237e+01, 19.98, 19.98, 19.98, 0., 0., 1., -360, 360],
+        [3., 4., 9.37500e-05, 8.4e-05, 1.13097336e+01, 19.98, 19.98, 19.98, 0., 0., 1., -360, 360],
+    ]))
+
+    assert emap == {
+        'Grid': {'etype': 'PQBus', 'idx': 0, 'static': {'vl': 110.0}},
+        'Bus0': {'etype': 'PQBus', 'idx': 1, 'static': {'vl': 20.0}},
+        'Bus1': {'etype': 'PQBus', 'idx': 2, 'static': {'vl': 20.0}},
+        'Bus2': {'etype': 'PQBus', 'idx': 3, 'static': {'vl': 20.0}},
+        'Bus3': {'etype': 'PQBus', 'idx': 4, 'static': {'vl': 20.0}},
+        'Trafo1': {'etype': 'Transformer', 'idx': 0, 'static': {
+            's_max': 40.0, 'prim_bus': 'Grid', 'sec_bus': 'Bus0'}},
+        'B_0': {'etype': 'Branch', 'idx': 1, 'static': {
+            's_max': 19.98, 'length': 5.0}},
+        'B_1': {'etype': 'Branch', 'idx': 2, 'static': {
+            's_max': 19.98, 'length': 3.0}},
+        'B_2': {'etype': 'Branch', 'idx': 3, 'static': {
+            's_max': 19.98, 'length': 2.0}},
+        'B_3': {'etype': 'Branch', 'idx': 4, 'static': {
+            's_max': 19.98, 'length': 0.3}},
+    }
