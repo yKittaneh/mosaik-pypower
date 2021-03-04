b9a0c9edbeb8d8f2c17a399f6abd2e77135e183e
Jan Sören Schwarz <schwarz@offis.de>
2021-03-04 18:30:17 +0100

    Bump version from 0.7.2 to 0.7.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mosaik-pypower',
-    version='0.7.2',
+    version='0.7.3',
     author='Stefan Scherfke',
     author_email='mosaik@offis.de',
     description='An adapter to use PYPOWER with mosaik.',
